aedf0c0f82ecec6007a1b8a0ceb06f6c4d0255fd
Mklein204 <149794313+Mklein204@users.noreply.github.com>
2023-11-10 10:20:36 +0100

    Parameterraum

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -6,27 +6,32 @@
 
 auto main(int argc, char **argv) -> int
 {
+    auto count = 20;
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
+
     CLI::App app{PROJECT_NAME};
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count",count, fmt::format("Create a vector with the given size default: {}", count));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
+    std::vector<int> data(count);
+
+    fmt::print("Created a vector with {} elements",count);
 
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
 
     /* INSERT YOUR CODE HERE */
 
