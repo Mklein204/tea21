cc7d65b1ed7044a4621f67be05ae7fb03805ee5a
Mklein204 <149794313+Mklein204@users.noreply.github.com>
2023-11-24 10:02:50 +0100

    Implementierung BMP

---
+++ exercise-004/bmp.cpp
@@ -0,0 +1,16 @@
+#include "bmp.h"
+
+bool BMP::read(const std::string& filename)
+{
+    auto ret = false;
+    //insert your code here
+    
+    return ret;
+}
+bool BMP::write(const std::string& filename)
+{
+    auto ret = false;
+    //insert your code here
+
+    return ret;
+}
--- exercise-004/main.cpp
+++ exercise-004/main.cpp
@@ -3,14 +3,11 @@
 
 #include "CLI/CLI.hpp"
 #include "config.h"
-
-class BMP {
-    bool read(const std::string& filename);
-    bool write(const std::string& filename);
-}
+#include "bmp.h"
 
 auto main(int argc, char **argv) -> int
 {
+    std::string filename(DEFAULT_BMP_FILE);
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -19,6 +16,7 @@ auto main(int argc, char **argv) -> int
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-f,--file", filename, fmt::format("The filename fpr the Bitmap, default: {}", filename));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -32,6 +30,7 @@ auto main(int argc, char **argv) -> int
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", app.get_name());
+    fmt::print("The filename to use: {}",filename);
 
     /* INSERT YOUR CODE HERE */
 
